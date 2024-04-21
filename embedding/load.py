f5c8882f796cd31136c342b31298b43e1bf92725
Michael Youssef <michael_youssef@brown.edu>
2024-04-21 18:53:01 -0400

    load.py

--- embedding/load.py
+++ embedding/load.py
@@ -1,31 +1,92 @@
-import torch
 import argparse
+from utils import *
+from torch_geometric.datasets import WikiCS, Amazon, Coauthor
 
-def wikics():
-    pass
+def wikics(args):
+    k = args.k
+    p_damp = args.p_damp
+    p_trunc = args.p_trunc
+    dropout = args.dropout
 
-def amazon_photo():
-    pass
+    # set optional parameters
+    if (p_damp is not None):
+        p_damp = 0.1
+    
+    if (p_trunc is not None):
+        p_trunc = 0.7
+    
+    graphs = WikiCS('../datasets/wikics')
 
-def coauthor_cs():
-    pass
+    generate_samples(
+        "./data/wikics",
+        graphs,
+        k,
+        p_damp,
+        p_trunc,
+        dropout
+    )
+
+def amazon_photo(args):
+    k = args.k
+    p_damp = args.p_damp
+    p_trunc = args.p_trunc
+    dropout = args.dropout
+
+    # set optional parameters
+    if (p_damp is not None):
+        p_damp = 0.1
+    
+    if (p_trunc is not None):
+        p_trunc = 0.7
+    
+    graphs = Amazon('../datasets/amazon_photo', 'photo')
+
+    generate_samples(
+        "./data/amazon_photo",
+        graphs,
+        k,
+        p_damp,
+        p_trunc,
+        dropout
+    )
+
+def coauthor_cs(args):
+    k = args.k
+    p_damp = args.p_damp
+    p_trunc = args.p_trunc
+    dropout = args.dropout
+
+    # set optional parameters
+    if (p_damp is not None):
+        p_damp = 0.35
+    
+    if (p_trunc is not None):
+        p_trunc = 0.7
+    
+    graphs = Coauthor('../datasets/coauthor_cs', 'CS')
+
+    generate_samples(
+        "./data/coauthor_cs",
+        graphs,
+        k,
+        p_damp,
+        p_trunc,
+        dropout
+    )
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', type=str, required=True, choices=['WikiCS', 'AmazonPhoto', 'CoauthorCS'])
     parser.add_argument('--k', type=int, default=2, required=False)
-    parser.add_argument('--p_damp', type=float, required=False)
-    parser.add_argument('--p_trunc', type=float, required=False)
-    parser.add_argument('--dropout', type=float, required=False)
-    parser.add_argument('--buffer_size', type=float, required=False)
+    parser.add_argument('--p_damp', type=float, default=None, required=False)
+    parser.add_argument('--p_trunc', type=float, default=None, required=False)
+    parser.add_argument('--dropout', type=float, default=0.1, required=False)
 
+    args = parser.parse_args()
 
-    # parser.add_argument('--p_e', type=float, required=True)
-    # parser.add_argument('--p_t', type=float, required=True)
-    # parser.add_argument('--max_subgraphs', type=int, required=True)
-    # parser.add_argument('--hidden_dimensions', type=float, required=True)
-    # parser.add_argument('--activation', type=str, required=True, choices=['PReLu', 'RReLu', 'ReLu'])
-    # parser.add_argument('--lr', type=float, required=True)
-    # parser.add_argument('--epochs', type=int, required=True)
-    # parser.add_argument('--bs', type=int, required=True)
-    # parser.add_argument('--logging', type=bool, required=True)
+    if (args.dataset == 'WikiCS'):
+        wikics(args)
+    elif (args.dataset == 'AmazonPhoto'):
+        amazon_photo(args)
+    else:
+        coauthor_cs(args)
